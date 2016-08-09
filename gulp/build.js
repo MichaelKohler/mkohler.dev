db1f27cfb8d6ef5a278dab3f9547d0749fe375b5
Michael Kohler <mkohler@picobudget.com>
2016-08-09 15:51:10 +0200

    copy fonts to fonts folder and remove flaticons

--- gulp/build.js
+++ gulp/build.js
@@ -67,9 +67,23 @@ gulp.task('html', ['inject', 'partials'], function () {
   });
 
 // Only applies for fonts from bower dependencies
-// Custom fonts are handled by the "other" task
+// Custom fonts are handled by the "otherFonts" task
 gulp.task('fonts', function () {
   return gulp.src($.mainBowerFiles())
+    .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+});
+
+gulp.task('otherFonts', function () {
+  var fileFilter = $.filter(function (file) {
+    return file.stat.isFile();
+  });
+
+  return gulp.src([
+    path.join(conf.paths.src, '/**/*')
+  ])
+    .pipe(fileFilter)
     .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
@@ -82,7 +96,7 @@ gulp.task('other', function () {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,js}')
+    path.join('!' + conf.paths.src, '/**/*.{html,css,js,eot,otf,ttf,woff,woff2}')
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
@@ -92,4 +106,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['html', 'fonts', 'otherFonts', 'other']);
