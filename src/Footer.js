9628ec9b220be5b252a96b27effbf5eba1f35dda
Michael Kohler <me@michaelkohler.info>
2019-03-20 00:12:52 +0100

    Add footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faXing } from '@fortawesome/free-brands-svg-icons';
+import {
+  faGlobe, faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Michael Kohler - Data accurate from 2013 onwards</p>
+    <footer className="padding-50 ui-bg-color02">
+      <div className="container-fluid nopadding">
+        <div className="row">
+          <div className="col-md-6">
+            <h4 className="font-accident-two-normal uppercase">Michael Kohler</h4>
+            <p className="inline-block">
+              JavaScript frontend and backend developer with a passion for Web Standards
+            </p>
+          </div>
+          <div className="col-md-6">
+            <h4 className="font-accident-two-normal uppercase">Follow me</h4>
+            <div className="follow">
+              <ul className="list-inline social">
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/KohlerSolutions"><FontAwesomeIcon icon={faTwitter} /></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.xing.com/profile/Michael_Kohler36"><FontAwesomeIcon icon={faXing} /></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.michaelkohler.info"><FontAwesomeIcon icon={faGlobe} /></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/MichaelKohler"><FontAwesomeIcon icon={faGithub} /></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="mailto:me@michaelkohler.info"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 copyrights">
+            <p>
+              © 2016-2019 Michael Kohler - Design is an adapted template by&nbsp;
+              <a href="https://wrapbootstrap.com/user/alexeyka">alexeyka</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
