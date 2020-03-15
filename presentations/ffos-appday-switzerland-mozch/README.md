adec19929754023ccba17e1b1fb83e8d3660ff87
MichaelKohler <MichaelKohler@users.noreply.github.com>
2020-03-15 17:24:27 +0000

    deploy: 587fcb093be0d70f6cc312a4136cb7c50cc79877

---
+++ presentations/ffos-appday-switzerland-mozch/README.md
@@ -0,0 +1,3 @@
+# Credits
+
+Original template from https://github.com/codepo8/mozilla-presentation-templates
---
+++ presentations/mozillaCH-how-mozilla-protect-web/slide_config.js
@@ -0,0 +1,16 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Why should you use Firefox?',
+    subtitle: 'You do!',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'imgs/firefox_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  }
+};
