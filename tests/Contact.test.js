4f4b21425b407a591faf0ea4c3241bb3416fb323
Michael Kohler <me@michaelkohler.info>
2020-11-08 01:44:26 +0100

    Remove Contact component

--- tests/Contact.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-import Contact from '../src/Contact';
-
-test('should correctly render Contact', () => {
-  const component = shallow(<Contact />);
-  expect(component).toMatchSnapshot();
-});
