3ecd30ef9c2e62e9b80d07a673946e17887bf094
Michael Kohler <me@michaelkohler.info>
2020-11-21 22:33:52 +0100

    Add LinkedIn profile to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faTwitter, faXing } from '@fortawesome/free-brands-svg-icons';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faXing,
+} from '@fortawesome/free-brands-svg-icons';
 import {
   faGlobe, faEnvelope,
 } from '@fortawesome/free-solid-svg-icons';
@@ -23,10 +28,11 @@ const Footer = () => {
         <div className="follow">
           <ul className="list-inline social">
             <li><a target="_blank" aria-label="Michael on Twitter" title="foo" rel="noopener noreferrer" href="https://www.twitter.com/KohlerSolutions"><FontAwesomeIcon icon={faTwitter} /></a></li>
-            <li><a target="_blank" aria-label="Michael on Xing" rel="noopener noreferrer" href="https://www.xing.com/profile/Michael_Kohler36"><FontAwesomeIcon icon={faXing} /></a></li>
+            <li><a target="_blank" aria-label="Michael's LinkedIn" rel="noopener noreferrer" href="https://www.linkedin.com/in/mkohlerdev"><FontAwesomeIcon icon={faLinkedin} /></a></li>
             <li><a target="_blank" aria-label="Michael's Blog" rel="noopener noreferrer" href="https://michaelkohler.info"><FontAwesomeIcon icon={faGlobe} /></a></li>
             <li><a target="_blank" aria-label="Michael on GitHub" rel="noopener noreferrer" href="https://www.github.com/MichaelKohler"><FontAwesomeIcon icon={faGithub} /></a></li>
             <li><a target="_blank" aria-label="Michael's Email" rel="noopener noreferrer" href="mailto:me@michaelkohler.info"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+            <li><a target="_blank" aria-label="Michael on Xing" rel="noopener noreferrer" href="https://www.xing.com/profile/Michael_Kohler36"><FontAwesomeIcon icon={faXing} /></a></li>
           </ul>
         </div>
       </section>
