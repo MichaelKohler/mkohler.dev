eed400212659f814b123780e71c4f2e4be181306
Michael Kohler <me@michaelkohler.info>
2021-10-12 00:53:58 +0200

    Update contributions data every hour

---
+++ fetch.js
@@ -0,0 +1,57 @@
+const debug = require('debug')('contributions:gather');
+const fs = require('fs').promises;
+
+const communityPortal = require('./lib/community-portal');
+const discourse = require('./lib/discourse');
+const github = require('./lib/github');
+const wiki = require('./lib/wiki');
+
+const CONTRIBUTION_FILE = `${__dirname}/src/_data/contributions.json`;
+
+async function fetchAll() {
+  debug('fetching...');
+  const existingContributions = JSON.parse(await fs.readFile(CONTRIBUTION_FILE, 'utf-8'));
+
+  const githubResult = await github.gather(existingContributions);
+  const wikiResult = await wiki.gather(existingContributions);
+  const communityPortalResult = await communityPortal.gather();
+  const discourseResult = await discourse.gather();
+
+  // Some types are fetched on every update run, therefore we want to filter them out for now.
+  // Otherwise we might end up with duplicate entries we'd need to filter.
+  // We need to make sure we always will save the "reps" source entries, as we do not fetch those
+  // anymore.
+  const ALWAYS_FULL_FETCH_SOURCE_ENTRIES = [
+    communityPortal.EVENTS_CATEGORY,
+    communityPortal.CAMPAIGNS_CATEGORY,
+    discourse.POSTS_CATEGORY,
+    discourse.TOPICS_CATEGORY,
+    wiki.WIKI_CATEGORY,
+  ];
+  const existingContributionsToSave = existingContributions.filter((activity) => !ALWAYS_FULL_FETCH_SOURCE_ENTRIES.includes(activity.source));
+  debug('ALREADY_EXISTING_CONTRIBUTIONS', existingContributionsToSave.length);
+
+  const uniqueContributions = Array.from(new Set([
+    ...existingContributionsToSave,
+    ...communityPortalResult, // always a new fetch (previous entries removed above)
+    ...discourseResult, // always a new fetch (previous entries removed above)
+    ...wikiResult, // always a new fetch (previous entries removed above)
+    ...githubResult,
+  ]));
+
+  if (uniqueContributions.length < existingContributions.length) {
+    debug('LESS_ACTIVITIES_THAN_BEFORE_ABORTING');
+    return;
+  }
+
+  if (uniqueContributions.length === existingContributions.length) {
+    debug('NO_UPDATE_ABORTING');
+    return;
+  }
+
+  uniqueContributions.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  await fs.writeFile(CONTRIBUTION_FILE, JSON.stringify(uniqueContributions));
+  debug('FILE_WRITTEN');
+}
+
+fetchAll();
