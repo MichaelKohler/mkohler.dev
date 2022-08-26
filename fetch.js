ff84a179e617260511bf450c0f63bcc62243a294
Michael Kohler <me@michaelkohler.info>
2022-08-26 16:41:06 +0200

    Update to latest contribution fetching package

--- fetch.js
+++ fetch.js
@@ -6,21 +6,16 @@ const config = {
     enabled: true,
     baseUrl: 'https://community.mozilla.org/people',
     username: 'mkohler',
-    participationType: 'Participated in an event',
-    campaignType: 'Participated in a campaign',
   },
   discourse: {
     enabled: true,
     baseUrl: 'https://discourse.mozilla.org',
     username: 'mkohler',
-    topicType: 'Created Discourse Topic',
-    postType: 'Posted on Discourse Topic',
   },
   mediaWiki: {
     enabled: true,
     baseUrl: 'https://wiki.mozilla.org/api.php?action=feedcontributions',
     username: 'michaelkohler',
-    editType: 'Wiki Edit',
     stopDate: '2012-05-01',
   },
   github: {
@@ -28,16 +23,6 @@ const config = {
     username: 'MichaelKohler',
     stopDate: '2008-01-01',
     filter: 'mozilla|common-voice|reps|remo|sc-scripts|webmaker|firefox|activate|surprisera|fxos|foxfooding|arewetenyet|asknot|community_dashboard_participation|appday',
-    types: {
-      commit: 'GitHub Commit',
-      privateCommit: 'Commit in private repository',
-      createdPR: 'Created PR',
-      createdIssue: 'Created Issue Report',
-      commentedPR: 'Commented on a Pull Request',
-      approvedPR: 'Approved a Pull Request',
-      changesRequestedPR: 'Requested changes on a Pull Request',
-      reviewedPR: 'Reviewed a Pull Request',
-    }
   },
 };
 
