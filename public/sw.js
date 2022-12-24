55222060a5b63b87e5bbf3de861c2e29c17a95e5
Michael Kohler <me@michaelkohler.info>
2022-12-24 16:28:13 +0100

    Simplify service worker

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,18 @@
-const CACHE_NAME = 'cache::v5';
+const CACHE_NAME = 'cache::v6';
+
+const RESOURCES_TO_PREFETCH = [
+  // We need the offline page so we can show it
+  // as it's unlikely somebody would fetch that
+  // by themselves..
+  '/offline/',
+];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        cache.addAll([
-          // General
-          '/data/resume.json',
-
-          // Subpages
-          '/offline/',
-          '/opensource/',
-          '/images/me.webp',
-          '/images/me_community_building.webp',
-          '/images/me_firefox.webp',
-          '/images/profile.webp',
-          '/images/reps_group.webp',
-          '/talks/',
-        ]);
-      })
+      .then((cache) => cache.addAll(RESOURCES_TO_PREFETCH))
       .catch(console.error)
   );
 });
@@ -27,6 +21,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
       .then((keyList) => {
+        // Purge old caches
         return Promise.all(keyList.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
@@ -44,12 +39,12 @@ self.addEventListener('fetch', (event) => {
       .then((res) => {
         return caches.open(CACHE_NAME)
           .then((cache) => {
-            cache.put(event.request.url, res.clone());
+            cache.put(event.request, res.clone());
             return res;
           });
       })
       .catch(() => {
-        return caches.match(event.request);
+        return caches.match(event.request)
       })
       .then((res) => {
         if (!res) {
