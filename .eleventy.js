53f8e21133d47b9ce632ffedea24494c16dabb52
Michael Kohler <me@michaelkohler.info>
2022-12-26 17:41:55 +0100

    Remove resume.json

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,6 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addPassthroughCopy({
     'public': '/',
-    'resume.json': 'data/resume.json'
   });
 
   eleventyConfig.setFrontMatterParsingOptions({
