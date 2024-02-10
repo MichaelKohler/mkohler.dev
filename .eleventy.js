0c0d051d400a1e69d71e6869b5d9508e686c7d6e
Michael Kohler <me@michaelkohler.info>
2024-02-11 00:23:50 +0100

    Adjust desktop styles to use nested CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,7 @@
 const pluginRss = require('@11ty/eleventy-plugin-rss');
-const CleanCSS = require('clean-css');
 const markdownIt = require('markdown-it');
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addFilter('cssmin', (code) => {
-    return new CleanCSS({}).minify(code).styles;
-  });
-
   eleventyConfig.addPassthroughCopy({
     'public': '/',
   });
