6eef066762299c407ddcc98f3517284a64ac0252
Michael Kohler <me@michaelkohler.info>
2024-10-03 19:25:14 +0200

    Forwared Accept header for payment pointer

--- api/pay.js
+++ api/pay.js
@@ -1,14 +1,23 @@
+const INTERNAL_SERVER_ERROR_CODE = 'INTERNAL_SERVER_ERROR';
+const PAYMENT_POINTER = "https://ilp.gatehub.net/941671735/EUR";
+
 export default async function handler(request, response) {
-  const res = await fetch("https://ilp.gatehub.net/941671735/EUR");
+  const acceptHeaderValue = request.headers.get('accept');
+
+  const res = await fetch(PAYMENT_POINTER, {
+    headers: {
+      Accept: acceptHeaderValue,
+    },
+  });
 
   if (!res.ok) {
-    return response.status(500).json({ error: 'INTERNAL_SERVER_ERROR' });
+    return response.status(500).json({ error: INTERNAL_SERVER_ERROR_CODE });
   }
 
   try {
     const data = await res.json();
     return response.status(200).json(data);
   } catch (error) {
-    return response.status(500).json({ error: 'INTERNAL_SERVER_ERROR' });
+    return response.status(500).json({ error: INTERNAL_SERVER_ERROR_CODE });
   }
 }
