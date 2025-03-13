d60d22c507b873bcb0636c708e355dabba8879bf
Michael Kohler <me@michaelkohler.info>
2025-03-13 10:42:23 +0100

    Fallback to JSON if not SPSP

--- api/pay.js
+++ api/pay.js
@@ -1,12 +1,19 @@
 const INTERNAL_SERVER_ERROR_CODE = 'INTERNAL_SERVER_ERROR';
 const PAYMENT_POINTER = "https://ilp.gatehub.net/941671735/EUR";
 
+const DEFAULT_ACCEPT_HEADER = "application/json";
+
 export default async function handler(request, response) {
-  const acceptHeaderValue = request.headers["accept"] ?? "application/json";
+  const acceptHeaderValue = request.headers["accept"];
+  const spspHeaders = [
+    'application/spsp4+json',
+    'application/spsp+json',
+  ];
+  const isSPSPRequest = spspHeaders.includes(acceptHeaderValue);
 
   const res = await fetch(PAYMENT_POINTER, {
     headers: {
-      Accept: acceptHeaderValue,
+      Accept: isSPSPRequest ? request.headers["accept"] : DEFAULT_ACCEPT_HEADER,
     },
   });
 
