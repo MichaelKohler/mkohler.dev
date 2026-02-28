caabfa49f87ece1a9776e23779afd1c048c9fd23
Michael Kohler <me@michaelkohler.info>
2026-02-28 10:23:53 +0100

    Revert "Remove Vercel specific code"
    This reverts commit b60afed797d3df7d6076173f2e2ffe1fa73d48dc.

--- public/main.js
+++ public/main.js
@@ -3,3 +3,12 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 };
+
+async function injectAnalytics() {
+  await import("/_vercel/insights/script.js");
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+}
+
+if (!navigator.globalPrivacyControl) {
+  injectAnalytics()
+}
